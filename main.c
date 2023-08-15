a29f673cadc0776f5bfaccb1897314de8d4d83ca
imadovetch <ibouderoua63@gmail.com>
2023-08-15 22:56:58 +0100

    && ||

--- main.c
+++ main.c
@@ -144,7 +144,7 @@ void execute_command_group(char **command_group, char *name, int *status) {
     }
 }
 
-void check_opperators(char **pcommands, char *name, int *status) {
+void check_semicollen(char **pcommands, char *name, int *status) {
     int i = 0;
     while (pcommands[i]) {
         char **command_group = NULL;
@@ -170,6 +170,46 @@ void check_opperators(char **pcommands, char *name, int *status) {
         free(command_group);
 
         i++; // Skip the semicolon if present
+    }
+}
+void check_opperators(char **pcommands, char *name, int *status) {
+    int i = 0;
+    while (pcommands[i]) {
+        int j = i;
+        int and_operator = 0; // To track the AND operator
+        int or_operator = 0;  // To track the OR operator
+
+        // Find the next AND or OR operator
+        while (pcommands[j] && strcmp(pcommands[j], "&&") != 0 && strcmp(pcommands[j], "||") != 0) {
+            j++;
+        }
+
+        if (pcommands[j]) {
+            if (strcmp(pcommands[j], "&&") == 0) {
+                and_operator = 1;
+            } else if (strcmp(pcommands[j], "||") == 0) {
+                or_operator = 1;
+            }
+
+            pcommands[j] = NULL; // Null-terminate the first command
+
+            // Execute the command
+            execute_command(name, pcommands[i], status);
+
+            if (*status == 0 && and_operator) {
+                // Execute the next command only if the previous one succeeded
+                execute_command(name, pcommands[j + 1], status);
+            } else if (*status != 0 && or_operator) {
+                // Execute the next command only if the previous one failed
+                execute_command(name, pcommands[j + 1], status);
+            }
+
+            i = j + 2; // Skip the operator and the next command
+        } else {
+            // No more operators found, execute the last command
+            execute_command(name, pcommands[i], status);
+            break;
+        }
     }
 }
 int main(int argc, char **argv) {
@@ -375,9 +415,18 @@ if (pid == 0) {
         commands = filter(argv[0],buffer);
         for(int h = 0;commands[h];h++)
         {
-            if (strcmp(commands[h],";") == 0)
+            if (strcmp(commands[h],"&&") == 0 || strcmp(commands[h] , "||") == 0)
             {
                 check_opperators(commands, argv[0], &status);
+                y = 1;
+            }
+                    
+        }
+        for(int h = 0;commands[h];h++)
+        {
+            if (strcmp(commands[h],";") == 0)
+            {
+                check_semicollen(commands, argv[0], &status);
                 y=1;
             }
                     
