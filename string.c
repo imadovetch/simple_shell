ce4faee44bf1cde63be62939b73c88099a15d949
ismailchiba <ismailchiba@gmail.com>
2023-08-16 23:01:34 +0100

    string functions

--- string.c
+++ string.c
@@ -29,3 +29,23 @@ void my_strcpy(char *dest, const char *src) {
         src++;
     }
 }
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first input
+ * @s2: second input
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; (s1[i] != '\0') && (s2[i] != '\0'); i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
