1c3d2c641dfbbf4218828d6be9c75fc03b5328ed
imadovetch <ibouderoua63@gmail.com>
2023-08-19 19:44:15 +0100

    making the main smaller

--- main.c
+++ main.c
@@ -231,11 +231,11 @@ int main(int argc, char **argv) {
 			printf("%s", prompt);
         
          if (argc > 1) {
-        // Non-interactive mode: Execute commands from file
+
         FILE *file = fopen(argv[1], "r");
 if (file == NULL) {
     fprintf(stderr, "%s: %d: Can't open %s\n", argv[0], line_number, argv[1]);
-    return 127; // Return an appropriate exit status
+    return 127;
 }
 
         char *line = NULL;
@@ -243,14 +243,11 @@ if (file == NULL) {
                    while (_getline(&line, &len, file) != -1) {
                size_t line_len = my_strlen(line);
                if (line_len > 0 && line[line_len - 1] == '\n') {
-                   line[line_len - 1] = '\0'; // Remove newline character
+                   line[line_len - 1] = '\0';
                }
-
-               // Split the line into individual commands
                char **commands = filter(argv[0], line);
 
                if (strcmp(commands[0], "cd") == 0) {
-    // Handle CD command
     if (commands[1] == NULL || strcmp(commands[1], "~") == 0) {
         if (chdir(home_dir) == -1) {
             perror("cd");
@@ -267,7 +264,6 @@ if (file == NULL) {
         }
     }
 
-    // Update PWD environment variable
     current_dir = getcwd(NULL, 0);
     if (current_dir != NULL) {
         setenv("PWD", current_dir, 1);
@@ -283,12 +279,12 @@ if (file == NULL) {
         else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$?") == 0) {
         printf("%d\n", status);
         status = 0;
-        continue;; // Display the exit status of the last command
+        continue;
         
         }else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$$") == 0) {
-    printf("%d\n", getpid()); // Use getpid() to print the PID of the shell
+    printf("%d\n", getpid());
     status = 0;
-    continue; // Display the PID of the shell
+    continue;
 }           else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$PATH") == 0) {
     char *path_value = getenv("PATH");
     if (path_value != NULL) {
@@ -319,70 +315,10 @@ if (file == NULL) {
                 continue;
             }
 
-          
-
-        if(isatty(STDIN_FILENO) == 0)
-        {  
-            pid_t pid = fork(); // Create a child process
-            
-      
-if (pid == 0) {
-    // Child process
-    char *command = commands[0];
-    char *full_path = NULL;
-
-    if (strchr(command, '/') == NULL) {
-        full_path = construct_full_path(argv[0], line_number, path, command);
-
-
-    }
-
-    if (full_path == NULL) {
-        full_path = command;
-    }
-    
-   if (execve(full_path, commands, environ) == -1) {
-    
-    if (errno == ENOENT) {
-        fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, command);
-        status = 127;
-    } else if (errno == EACCES) {
-        fprintf(stderr, "sh: %s: permission denied\n", command);
-        status = 2;
-    } else {
-        perror("execve");
-        status = 1; // Set an appropriate status for other errors
-    }
-    
-   
-}
-
-} else if (pid > 0) {
-    // Parent process
-    wait(&status); // Wait for the child process to finish
-    if (WIFEXITED(status)) {
-        status = WEXITSTATUS(status); // Get the exit status of the child process
-    } else {
-        // Child process did not terminate normally
-        status = 1; // Set an appropriate non-zero status
-    }
-} else {
-    perror("fork"); // Print an error if forking failed
-    status = 1; // Set an appropriate non-zero status
-}
-
-                   // Clean up allocated memory for commands
-                   for (int i = 0; commands[i] != NULL; i++) {
-                       free(commands[i]);
-                   }
-                   free(commands);
-               }
            }
 
            free(line);
            fclose(file);
-
-           // Exit the shell after processing the file
            return status;}
 
         x = _getline(&buffer, &size, stdin);
@@ -403,7 +339,7 @@ if (pid == 0) {
     if (!isspace(buffer[i])) {
        
         break;
-    }//dakhel
+    }
     
   
     if (buffer[i + 1] == '\0') {
@@ -441,9 +377,7 @@ if (pid == 0) {
         remove_comments(commands);
         
         if (commands[0] != NULL) {
-        //adjust_ls_command(&commands[0]);
          if (strcmp(commands[0], "cd") == 0) {
-    // Handle CD command
     if (commands[1] == NULL || strcmp(commands[1], "~") == 0) {
         if (chdir(home_dir) == -1) {
             perror("cd");
@@ -459,8 +393,6 @@ if (pid == 0) {
             fprintf(stderr, "%s: %d: cd: can't cd to %s\n", argv[0], line_number, commands[1]);
         }
     }
-
-    // Update PWD environment variable
     current_dir = getcwd(NULL, 0);
     if (current_dir != NULL) {
         setenv("PWD", current_dir, 1);
@@ -476,12 +408,12 @@ if (pid == 0) {
         else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$?") == 0) {
         printf("%d\n", status);
         status = 0;
-        continue;; // Display the exit status of the last command
+        continue;
         
         }else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$$") == 0) {
-    printf("%d\n", getpid()); // Use getpid() to print the PID of the shell
+    printf("%d\n", getpid());
     status = 0;
-    continue; // Display the PID of the shell
+    continue;
 }           else if (strcmp(commands[0], "echo") == 0 && strcmp(commands[1], "$PATH") == 0) {
     char *path_value = getenv("PATH");
     if (path_value != NULL) {
@@ -514,67 +446,16 @@ if (pid == 0) {
 }
           
 
-        if(isatty(STDIN_FILENO) == 0)
-        {  
-            pid_t pid = fork(); // Create a child process
-            
+       
+                  
+       pid_t pid = fork();
       
 if (pid == 0) {
-    // Child process
     char *command = commands[0];
     char *full_path = NULL;
 
     if (strchr(command, '/') == NULL) {
-        full_path = construct_full_path(argv[0], line_number, path, command);
-
-
-    }
-
-    if (full_path == NULL) {
-        full_path = command;
-    }
-    
-   if (execve(full_path, commands, environ) == -1) {
-    
-    if (errno == ENOENT) {
-        fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, command);
-        status = 127;
-    } else if (errno == EACCES) {
-        fprintf(stderr, "sh: %s: permission denied\n", command);
-        status = 2;
-    } else {
-        perror("execve");
-        status = 1; // Set an appropriate status for other errors
-    }
-    
-   
-}
-
-} else if (pid > 0) {
-    // Parent process
-    wait(&status); // Wait for the child process to finish
-    if (WIFEXITED(status)) {
-        status = WEXITSTATUS(status); // Get the exit status of the child process
-    } else {
-        // Child process did not terminate normally
-        status = 1; // Set an appropriate non-zero status
-    }
-} else {
-    perror("fork"); // Print an error if forking failed
-    status = 1; // Set an appropriate non-zero status
-}
-   atexit(cleanupFunction);}
-             
- if(isatty(STDIN_FILENO) != 0){      
-       pid_t pid = fork(); // Create a child process
-      
-if (pid == 0) {
-    // Child process
-    char *command = commands[0];
-    char *full_path = NULL;
-
-    if (strchr(command, '/') == NULL) {
-        full_path = construct_full_path(argv[0], __LINE__, path, command);
+        full_path = construct_full_path(argv[0],line_number , path, command);
 
 
     }
@@ -592,35 +473,34 @@ if (pid == 0) {
         status = 2;
     } else {
         perror("execve");
-        status = 1; // Set an appropriate status for other errors
+        status = 1; 
     }
     
-    ; // Exit the child process with the updated status
+    ; 
 }
 
 
 
 } else if (pid > 0) {
-    // Parent process
-    wait(&status); // Wait for the child process to finish
+    wait(&status);
     if (WIFEXITED(status)) {
-        status = WEXITSTATUS(status); // Get the exit status of the child process
+        status = WEXITSTATUS(status);
     } else {
-        // Child process did not terminate normally
-        status = 1; // Set an appropriate non-zero status
+        status = 1;
     }
 } else {
-    perror("fork"); // Print an error if forking failed
-    status = 1; // Set an appropriate non-zero status
+    perror("fork");
+    status = 1;
 }
-    }}
+if(isatty(STDIN_FILENO) == 0)
+        { atexit(cleanupFunction);}
+    }
 
     for (int i = 0; commands[i] != NULL; i++) {
         free(commands[i]);
     }
     free(commands);
-    //free(buffer);
+    free(buffer);
     exit(status);
     return 0;
-}//asel
-// commantit comand == null
+}
