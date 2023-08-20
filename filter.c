8958641881cdc332a524449cf05e080c973d6d2f
ismailchiba <ismailchiba0@gmail.com>
2023-08-20 21:25:27 +0100

    filter.c update

--- filter.c
+++ filter.c
@@ -1,49 +1,61 @@
 #include "main.h"
 
 
-char *_strtok(s, delim)
-	register char *s;
-	register const char *delim;
+/**
+ * _strtok - Tokenizes a string based on delimiters.
+ * @s: The input string.
+ * @delim: Delimiter characters.
+ *
+ * Return: Pointer to the next token or NULL if no more tokens.
+ */
+char *_strtok(register char *s, register const char *delim)
 {
-	register char *spanp;
-	register int c, sc;
-	char *tok;
-	static char *last;
+    register char *spanp;
+    register int c, sc;
+    char *tok;
+    static char *last;
 
-
-	if (s == NULL && (s = last) == NULL)
-		return (NULL);
+    if (s == NULL && (s = last) == NULL)
+        return (NULL);
 
 cont:
-	c = *s++;
-	for (spanp = (char *)delim; (sc = *spanp++) != 0;) {
-		if (c == sc)
-			goto cont;
-	}
+    c = *s++;
+    for (spanp = (char *)delim; (sc = *spanp++) != 0;) {
+        if (c == sc)
+            goto cont;
+    }
 
-	if (c == 0) {		
-		last = NULL;
-		return (NULL);
-	}
-	tok = s - 1;
+    if (c == 0) {
+        last = NULL;
+        return (NULL);
+    }
+    tok = s - 1;
 
-	for (;;) {
-		c = *s++;
-		spanp = (char *)delim;
-		do {
-			if ((sc = *spanp++) == c) {
-				if (c == 0)
-					s = NULL;
-				else
-					s[-1] = 0;
-				last = s;
-				return (tok);
-			}
-		} while (sc != 0);
-	}
-	
+    for (;;) {
+        c = *s++;
+        spanp = (char *)delim;
+        do {
+            if ((sc = *spanp++) == c) {
+                if (c == 0)
+                    s = NULL;
+                else
+                    s[-1] = 0;
+                last = s;
+                return (tok);
+            }
+        } while (sc != 0);
+    }
 }
-char **filter(const char *program_name, char *str) {
+
+/**
+ * filter - Tokenizes a string and filters commands.
+ * @program_name: Name of the program.
+ * @str: Input string containing commands.
+ *
+ * Return: Array of command strings.
+ */
+char **filter(const char *program_name, char *str)
+{
     char *com, **commands = NULL;
     int i = 0;
 
@@ -67,41 +79,38 @@ char **filter(const char *program_name, char *str) {
 
     for (i = 0; commands[i]; i++) {
         if (commands[i] && commands[i + 1]) {
-            if (strcmp(commands[i], "exit") == 0) {
+            if (_strcmp(commands[i], "exit") == 0) {
                 char *endptr;
                 int x = strtol(commands[i + 1], &endptr, 10);
                 if (*endptr != '\0') {
-                    fprintf(stderr, "%s: 1: exit: Illegal number: %s\n", program_name, commands[i + 1]);
-                    _free(commands);free(commands);
+                    fprintf(stderr, "%s: 1: exit: Illegal number: %s\n",
+                            program_name, commands[i + 1]);
+                    _free(commands);
+                    free(commands);
                     exit(2);
                 } else if (x <= 0) {
-                    fprintf(stderr, "%s: 1: exit: Illegal number: %d\n", program_name, x);
-                    _free(commands);free(commands);
+                    fprintf(stderr, "%s: 1: exit: Illegal number: %d\n",
+                            program_name, x);
+                    _free(commands);
+                    free(commands);
                     exit(2);
                 } else if (x >= 255) {
-                    _free(commands);free(commands);
+                    _free(commands);
+                    free(commands);
                     exit(232);
                 }
 
-                _free(commands);free(commands);
+                _free(commands);
+                free(commands);
                 exit((int)x);
             }
-        } else if (strcmp(commands[i], "exit") == 0) {
-            _free(commands);free(commands);
+        } else if (_strcmp(commands[i], "exit") == 0) {
+            _free(commands);
+            free(commands);
             exit(0);
         }
     }
 
     commands[i] = NULL;
     return commands;
-}
-int _free(char **p)
-{
-	if (p && *p)
-	{
-		free(*p);
-		*p = NULL;
-		return (1);
-	}
-	return (0);
 }
