41022a5cb5ec85991fd63197ee97fafb0925c6f6
ismailchiba <ismailchiba0@gmail.com>
2023-08-21 22:05:19 +0100

    list_func1.c betty

--- list_func1.c
+++ list_func1.c
@@ -7,7 +7,8 @@
  * This function checks if the command contains "ls" and adjusts the command
  * pointer to point to the first occurrence of "ls" within the command.
  *
- * @command should point to a valid string. If the command does not contain "ls",
+ * @command should point to a valid string.
+ *  If the command does not contain "ls",
  * the pointer remains unchanged.
  */
 void adjust_ls_command(char **command)
@@ -25,22 +26,24 @@ void adjust_ls_command(char **command)
  * @dir: Directory to search for the command
  * @command: Command to construct a path for
  *
- * Return: A pointer to the dynamically allocated full path string, or NULL on failure.
+ * Return: A pointer to the dynamically allocated full path string,
+ *  or NULL on failure.
  */
-char *construct_full_path(const char *program_name, int line_number, const char *dir, const char *command)
+char *construct_full_path(const char *program_name,
+int line_number, const char *dir, const char *command)
 {
 	size_t dir_len = my_strlen(dir);
 	size_t command_len = my_strlen(command);
 	size_t path_len = dir_len + 1 + command_len + 1;
 
 	char *full_path = malloc(path_len);
+
 	if (full_path == NULL)
 	{
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 
-	/* If the command is an absolute path or contains any slashes, use it directly */
 	if (command[0] == '/' || strchr(command, '/') != NULL)
 	{
 		my_strcpy(full_path, command);
@@ -52,19 +55,18 @@ char *construct_full_path(const char *program_name, int line_number, const char 
 		full_path[dir_len] = '/';
 		my_strcpy(full_path + dir_len + 1, command);
 	}
-	
 	full_path[path_len - 1] = '\0';
 
 	/* Check if the file exists */
 	if (access(full_path, F_OK) == -1)
 	{
 		/* File does not exist */
-		fprintf(stderr, "%s: %d: %s: not found\n", program_name, line_number, command);
+		fprintf(stderr, "%s: %d: %s: not found\n",
+		 program_name, line_number, command);
 		free(full_path);
 		exit(127);
 	}
-
-	return full_path;
+	return (full_path);
 }
 /**
  * _getline - Read a line from a file stream
@@ -82,7 +84,7 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 		*lineptr = (char *)malloc(*n);
 		if (*lineptr == NULL)
 		{
-			return -1;  /* Error allocating memory */
+			return (-1);  /* Error allocating memory */
 		}
 	}
 
@@ -97,27 +99,20 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 		{
 			*n *= 2;  /* Double the buffer size */
 			char *new_ptr = (char *)realloc(*lineptr, *n);
+
 			if (new_ptr == NULL)
 			{
-				return -1;  /* Error reallocating memory */
+				return (-1);  /* Error reallocating memory */
 			}
 			*lineptr = new_ptr;
 		}
-
 		if (c == '\n')
-		{
 			break;  /* Reached end of line */
-		}
 	}
-
 	(*lineptr)[pos] = '\0';  /* Null-terminate the string */
-
 	if (pos == 0 && c == EOF)
-	{
-		return -1;  /* No characters read, reached end of file */
-	}
-
-	return pos;  /* Return the number of characters read */
+		return (-1);  /* No characters read, reached end of file */
+	return (pos);  /* Return the number of characters read */
 }
 
 /**
@@ -126,19 +121,21 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
  *
  * This function iterates through the array of command strings and removes
  * any comments by null-terminating the string at the '#' character. If a
- * line consists entirely of a comment, the corresponding command is set to NULL.
+ * line consists entirely of a comment,
+ *  the corresponding command is set to NULL.
  */
 void remove_comments(char **commands)
 {
 	for (int i = 0; commands[i] != NULL; i++)
 	{
 		char *hash_pos = strchr(commands[i], '#');
+
 		if (hash_pos != NULL)
 		{
-			*hash_pos = '\0'; /* Null-terminate the command at the '#' character */
+			*hash_pos = '\0';
 			if (hash_pos == commands[i])
 			{
-				commands[i] = NULL; /* If the entire command is a comment, remove it */
+				commands[i] = NULL;
 			}
 		}
 	}
@@ -151,16 +148,18 @@ void remove_comments(char **commands)
  * @status: Pointer to store the exit status
  *
  * This function forks a child process and executes the specified command using
- * the execvp system call. It waits for the child process to complete and stores
+ * the execvp system call.It waits for the child process to complete and stores
  * the exit status in the variable pointed to by @status.
  */
 void execute_command(char *program_name, char *command, int *status)
 {
 	pid_t pid = fork();
+
 	if (pid == 0)
 	{
 		/* Child process */
 		char *args[] = {command, NULL};
+
 		c_execvp(command, args);
 
 		/* If execvp returns, the command was not found */
@@ -169,7 +168,7 @@ void execute_command(char *program_name, char *command, int *status)
 	}
 	else if (pid > 0)
 	{
-		waitpid(pid, status, 0); /* Wait for the child process and store the exit status */
+		waitpid(pid, status, 0);
 	}
 	else
 	{
