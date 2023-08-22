f72d98fd56f0eb6f353d911dbba82b5c447fdd87
imadovetch <ibouderoua63@gmail.com>
2023-08-22 04:02:31 +0100

    handling some betties

--- main.c
+++ main.c
@@ -1,191 +1,180 @@
 #include "main.h"
 
-
-
-
-
-
-
-
-
-
 int status = 0;
-
-
-
-
-
 
 void cleanupFunction()
 {
 	exit(status);
 }
-
-
-
-
-
-
-
-
-int main(int argc, char **argv) {
-   int line_number = 0;extern char **environ;
-    char *prompt = "mine$ ";
-    char *buffer = NULL;
-    size_t size = 0;
-    int x = 0;
-    char **commands = NULL;
-    char *path = "/usr/bin";
-    char *home_dir = getenv("HOME");
-    char *previous_dir = NULL;
-    char *current_dir = NULL;
-    while (1) {
-		if(isatty(STDIN_FILENO) != 0)
-			printf("%s", prompt);
-        
-         if (argc > 1) {
-
-        FILE *file = fopen(argv[1], "r");
-if (file == NULL) {
-    fprintf(stderr, "%s: %d: Can't open %s\n", argv[0], line_number, argv[1]);
-    return 127;
-}
-        char *line = NULL;
-        size_t len = 0;
-                   while (_getline(&line, &len, file) != -1) {
-               size_t line_len = my_strlen(line);
-               if (line_len > 0 && line[line_len - 1] == '\n') {
-                   line[line_len - 1] = '\0';
-               }
-               char **commands = filter(argv[0], line);
-
-               if (commands[0] != NULL) {
-            if (_strcmp(commands[0], "cd") == 0) {
-                handle_cd(commands, home_dir, &previous_dir,argv[0]);
-                continue;
-            } else if (_strcmp(commands[0], "echo") == 0) {
-                handle_echo(commands, &status);
-                continue;
-            } else if (_strcmp(commands[0], "setenv") == 0 || _strcmp(commands[0], "unsetenv") == 0) {
-                handle_environment(commands, &status);
-                continue;
-            }
-        }
-           }
-           free(line);
-           fclose(file);
-           return status;}
-        x = _getline(&buffer, &size, stdin);  
-		line_number++;
-        if (x == -1) { 
-            free(buffer);/*found it */
-            return 0;
-        }
- 
-        size_t len = my_strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
-        for (int i = 0; buffer[i]; i++) {
-    if (!isspace(buffer[i])) {
-        break;
+int handlers(char **commands, char *home_dir, char **previous_dir, char *program_name)
+{
+    if (_strcmp(commands[0], "cd") == 0)
+    {
+        handle_cd(commands, home_dir, previous_dir, program_name);
+        return 1;
     }
-    if (buffer[i + 1] == '\0') {
-        exit(0);
+    else if (_strcmp(commands[0], "echo") == 0)
+    {
+        handle_echo(commands, &status);
+        return 1;
     }
-}
-       int y = 0;
-        commands = filter(argv[0],buffer);
-        for(int h = 0;commands[h];h++)
-        {
-            if (_strcmp(commands[h],"&&") == 0 || _strcmp(commands[h] , "||") == 0)
-            {
-                check_opperators(commands, argv[0], &status);
-                y = 1;
-            }
-                    
-        }
-        for(int h = 0;commands[h];h++)
-        {
-            if (_strcmp(commands[h],";") == 0)
-            {
-                check_semicollen(commands, argv[0], &status);
-                y=1;
-            }       
-        }
-        if (y == 1)
-            continue;
-        
-       if (commands[0] != NULL && _strcmp(commands[0], "#") == 0 && isatty(STDIN_FILENO) == 0) {
-        free(commands);/*non tested*/
-        free(buffer);/*free done */
-    exit(0);
-}
-        remove_comments(commands);
-        if (commands[0] != NULL) {
-            if (_strcmp(commands[0], "cd") == 0) {
-                handle_cd(commands, home_dir, &previous_dir,argv[0]);
-                continue;
-            } else if (_strcmp(commands[0], "echo") == 0) {
-                handle_echo(commands, &status);
-                continue;
-            } else if (_strcmp(commands[0], "setenv") == 0 || _strcmp(commands[0], "unsetenv") == 0) {
-                handle_environment(commands, &status);
-                continue;
-            }
-        }      
-       pid_t pid = fork();
-      
-if (pid == 0) {
-    char *command = commands[0];
-    char *full_path = NULL;
-
-    if (strchr(command, '/') == NULL) {
-        full_path = construct_full_path(argv[0],line_number , path, command);
-
-
+    else if (_strcmp(commands[0], "setenv") == 0 || _strcmp(commands[0], "unsetenv") == 0)
+    {
+        handle_environment(commands, &status);
+        return 1;
     }
-
-    if (full_path == NULL) {
-        full_path = command;
-    }
-    
-   if (execve(full_path, commands, environ) == -1) {
-    if (errno == ENOENT) {
-        fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, command);
-        status = 127;
-    } else if (errno == EACCES) {
-        fprintf(stderr, "sh: %s: permission denied\n", command);
-        status = 2;
-    } else {
-        perror("execve");
-        status = 1; 
-    }
-    
-    ; 
-}
-} else if (pid > 0) {
-    wait(&status);
-    if (WIFEXITED(status)) {
-        status = WEXITSTATUS(status);
-    } else {
-        status = 1;
-    }
-} else {
-    perror("fork");
-    status = 1;
-}
-if(isatty(STDIN_FILENO) == 0)
-        { atexit(cleanupFunction);}
-        
-    free(commands);
-    }
-
-    for (int i = 0; commands[i] != NULL; i++) {
-        free(commands[i]);
-    }
-    free(commands);
-    free(buffer);
-    exit(status);
     return 0;
+}
+int main(int argc, char **argv)
+{
+	int line_number = 0;
+	extern char **environ;
+	char *prompt = "mine$ ";
+	char *buffer = NULL;
+	size_t size = 0;
+	int x = 0;
+	char **commands = NULL;
+	char *path = "/usr/bin";
+	char *home_dir = getenv("HOME");
+	char *previous_dir = NULL;
+	char *current_dir = NULL;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) != 0)
+			printf("%s", prompt);
+		if (argc > 1)
+		{
+			FILE *file = fopen(argv[1], "r");
+			if (file == NULL)
+			{
+				fprintf(stderr, "%s: %d: Can't open %s\n", argv[0], line_number, argv[1]);
+				return (127);
+			}
+			char *line = NULL;
+			size_t len = 0;
+			while (_getline(&line, &len, file) != -1)
+			{
+				size_t line_len = my_strlen(line);
+				if (line_len > 0 && line[line_len - 1] == '\n')
+					line[line_len - 1] = '\0';
+				char **commands = filter(argv[0], line);
+				if (commands[0] != NULL)
+				{
+                    if (handlers(commands, home_dir, &previous_dir, argv[0]))
+                        continue;
+				}
+			}
+			free(line);
+			fclose(file);
+			return (status);
+		}
+
+		x = _getline(&buffer, &size, stdin);
+		line_number++;
+		if (x == -1)
+		{
+			free(buffer);
+			return (0);
+		}
+		size_t len = my_strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n')
+		{
+			buffer[len - 1] = '\0';
+		}
+		for (int i = 0; buffer[i]; i++)
+		{
+			if (!isspace(buffer[i]))
+				break;
+			if (buffer[i + 1] == '\0')
+				exit(0);
+		}
+
+		int y = 0;
+		commands = filter(argv[0], buffer);
+
+		for (int h = 0; commands[h]; h++)
+		{
+			if (_strcmp(commands[h], "&&") == 0 || _strcmp(commands[h], "||") == 0)
+			{
+				check_opperators(commands, argv[0], &status);
+				y = 1;
+			}
+		}
+
+		for (int h = 0; commands[h]; h++)
+		{
+			if (_strcmp(commands[h], ";") == 0)
+			{
+				check_semicollen(commands, argv[0], &status);
+				y = 1;
+			}
+		}
+		if (y == 1)
+			continue;
+		if (commands[0] != NULL && _strcmp(commands[0], "#") == 0 && isatty(STDIN_FILENO) == 0)
+		{
+			free(commands);
+			free(buffer);
+			exit(0);
+		}
+		remove_comments(commands);
+		if (commands[0] != NULL)
+		{
+            if (handlers(commands, home_dir, &previous_dir, argv[0]))
+                continue;
+		}
+		pid_t pid = fork();
+		if (pid == 0)
+		{
+			char *command = commands[0];
+			char *full_path = NULL;
+
+			if (strchr(command, '/') == NULL)
+				full_path = construct_full_path(argv[0], line_number, path, command);
+			if (full_path == NULL)
+				full_path = command;
+			if (execve(full_path, commands, environ) == -1)
+			{
+				if (errno == ENOENT)
+				{
+					fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, command);
+					status = 127;
+				}
+				else if (errno == EACCES)
+				{
+					fprintf(stderr, "sh: %s: permission denied\n", command);
+					status = 2;
+				}
+				else
+				{
+					perror("execve");
+					status = 1;
+				}
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+			if (WIFEXITED(status))
+				status = WEXITSTATUS(status);
+			else
+				status = 1;
+		}
+		else
+		{
+			perror("fork");
+			status = 1;
+		}
+
+		if (isatty(STDIN_FILENO) == 0)
+			atexit(cleanupFunction);
+		free(commands);
+	}
+	for (int i = 0; commands[i] != NULL; i++)
+		free(commands[i]);
+	free(commands);
+	free(buffer);
+	exit(status);
+	return (0);
 }
