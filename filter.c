b4dab077150b3700d7d80ac2b505724b72ef4902
imadovetch <ibouderoua63@gmail.com>
2023-08-23 19:45:00 +0100

    final

--- filter.c
+++ filter.c
@@ -16,6 +16,7 @@ char *_strtok(register char *s, register const char *delim)
 
 	if (s == NULL && (s = last) == NULL)
 		return (NULL);
+
 cont:
 	c = *s++;
 	for (spanp = (char *)delim; (sc = *spanp++) != 0;)
@@ -30,6 +31,7 @@ cont:
 		return (NULL);
 	}
 	tok = s - 1;
+
 	for (;;)
     {
 		c = *s++;
@@ -57,43 +59,71 @@ cont:
  */
 char **filter(const char *program_name, char *str)
 {
-    char *com, **commands = NULL;
-    int i = 0;
+	char *com, **commands = NULL;
+	int i = 0;
 
-    com = _strtok(str, " \n\t");
-    while (com)
+	com = _strtok(str, " \n\t");
+	while (com)
     {
-        commands = realloc(commands, (i + 1) * sizeof(char *));
-        commands[i++] = com;
-        com = _strtok(NULL, " \t\n");
-    }
-    commands = realloc(commands, (i + 1) * sizeof(char *));
-    commands[i] = NULL;
-
-    for (i = 0; commands[i]; i++)
-    {
-        if (commands[i] && commands[i + 1] && _strcmp(commands[i], "exit") == 0)
+		commands = realloc(commands, (i + 1) * sizeof(char *));
+		if (!commands)
         {
-            char *endptr;
-            int x = strtol(commands[i + 1], &endptr, 10);
-            if (*endptr != '\0' || x <= 0 || x >= 255)
+			perror("realloc failed");
+			exit(98);
+		}
+		commands[i] = com;
+		i++;
+		com = _strtok(NULL, " \t\n");
+	}
+	commands = realloc(commands, (i + 1) * sizeof(char *));
+	if (!commands)
+    {
+		perror("realloc failed");
+		exit(98);
+	}
+	commands[i] = NULL;
+
+	for (i = 0; commands[i]; i++)
+    {
+		if (commands[i] && commands[i + 1])
+        {
+			if (_strcmp(commands[i], "exit") == 0)
             {
-                fprintf(stderr, "%s: 1: exit: Illegal number: %s\n", program_name, commands[i + 1]);
-                _free(commands);
-                free(commands);
-                exit(*endptr != '\0' ? 2 : (x >= 255 ? 232 : x));
-            }
-            _free(commands);
-            free(commands);
-            exit(x);
-        }
-        else if (_strcmp(commands[i], "exit") == 0)
+				char *endptr;
+				int x = strtol(commands[i + 1], &endptr, 10);
+				if (*endptr != '\0')
+                {
+					fprintf(stderr, "%s: 1: exit: Illegal number: %s\n",
+							program_name, commands[i + 1]);
+					_free(commands);
+					free(commands);
+					exit(2);
+				} else if (x <= 0)
+                {
+					fprintf(stderr, "%s: 1: exit: Illegal number: %d\n",
+							program_name, x);
+					_free(commands);
+					free(commands);
+					exit(2);
+				} else if (x >= 255)
+                {
+					_free(commands);
+					free(commands);
+					exit(232);
+				}
+
+				_free(commands);
+				free(commands);
+				exit((int)x);
+			}
+		} else if (_strcmp(commands[i], "exit") == 0)
         {
-            _free(commands);
-            free(commands);
-            exit(0);
-        }
-    }
-    commands[i] = NULL;
-    return commands;
+			_free(commands);
+			free(commands);
+			exit(0);
+		}
+	}
+
+	commands[i] = NULL;
+	return commands;
 }
