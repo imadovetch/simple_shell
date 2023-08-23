842e80e23e403a1704ed14e0525eb64d931f85ea
ismailchiba <ismailchiba@gmail.com>
2023-08-23 01:09:25 +0100

    main.h

--- main.h
+++ main.h
@@ -14,10 +14,10 @@
 #include <stddef.h>
 
 /* protos */
-char *_strtok(s, delim);
-char **filter(const char *program_name,char *str );
+char *_strtok(register char *s, register const char *delim);
+char **filter(const char *program_name, char *str);
 int _snprintf(char *str, size_t size, const char *format, ...);
-//string functions protos
+/*string functions protos*/
 size_t my_strlen(const char *str);
 void my_strcpy(char *dest, const char *src);
 int _strcmp(char *s1, char *s2);
@@ -27,12 +27,14 @@ void string_free(char **ptr);
 void *my_realloc(void *ptr, size_t new_size);
 char *_strdup(char *str);
 void adjust_ls_command(char **command);
-char *construct_full_path(const char *program_name, int line_number, const char *dir, const char *command);
+char *construct_full_path(const char *program_name, int line_number,
+const char *dir, const char *command);
 int c_execvp(const char *file, char *const argv[]);
 void execute_command(char *program_name, char *command, int *status);
 void execute_command_group(char **command_group, char *name, int *status);
 void check_semicollen(char **pcommands, char *name, int *status);
-void handle_cd(char **commands, char *home_dir, char **previous_dir, char *name);
+void handle_cd(char **commands, char *home_dir,
+char **previous_dir, char *name);
 void handle_echo(char **commands, int *status);
 void handle_environment(char **commands, int *status);
 
