8dc390458f9e115d23913e349a587972bd9d5425
imadovetch <ibouderoua63@gmail.com>
2023-08-23 19:00:30 +0100

    try betty 1

--- handlers_func.c
+++ handlers_func.c
@@ -86,7 +86,7 @@ void handle_echo(char **commands, int *status)
 		{
 			fprintf(stderr, "PATH environment variable not set\n");
 		}
-	}else
+	} else
 		printf("%s\n", commands[1]);
 	*status = 0;
 }
